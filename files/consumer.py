c67cf63b4005278dd1287ffb5f51ba07f8904519
Gyuho Shim <gjshim@wisc.edu>
2023-12-06 23:40:00 +0000

    Gyuho

--- files/consumer.py
+++ files/consumer.py
@@ -6,10 +6,12 @@ import report_pb2
 import sys
 
 def update_stats(stats, report):
+    prev_date=report.date
     if report.date < stats['start']:
         stats['start'] = report.date
     if report.date > stats['end']:
         stats['end'] = report.date
+        
     stats['count'] += 1
     stats['sum'] += report.degrees
     stats['avg'] = stats['sum'] / stats['count']
@@ -41,20 +43,29 @@ def main():
     consumer.assign(topic_partitions)
     
     partition_data = {p: load_partition_data(p) for p in partitions}
-
+    date_frequency = {}
     for tp in topic_partitions:
         consumer.seek(tp, partition_data[tp.partition]['offset'])
-
+    #temp_date=0
     for message in consumer:
         report = message.value
         data = partition_data[message.partition]
         month = message.key.decode()
         year = report.date.split('-')[0]
-
+       # if report.date <= temp_date:
+        #    continue
+        #temp_date=report.date
         if month not in data:
             data[month] = {}
         if year not in data[month]:
             data[month][year] = {"count": 0, "sum": 0, "avg": 0, "start": report.date, "end": report.date}
+        current_stats=data[month][year]
+        if report.date in date_frequency:
+            print(f"Skipping duplicate date {report.date} in partition {message.partition}")
+            continue
+
+        # Update the date frequency dictionary
+        date_frequency[report.date] = date_frequency.get(report.date, 0) + 1
 
         update_stats(data[month][year], report)
         data['offset'] = message.offset + 1
